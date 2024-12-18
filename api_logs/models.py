d40946bc067d679d291bcd987695fca712bb56d7
IngEnigma <zs220199@estudiantes.uv.mx>
2024-12-17 23:36:58 -0600

    model added

--- api_logs/models.py
+++ api_logs/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class APILog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)  
     username = models.CharField(max_length=255)  
+    model = models.CharField(max_length=50)
     request_data = models.TextField()  
     response_data = models.TextField() 
 
     def __str__(self):
-        return f"Log {self.id} - {self.username} - {self.timestamp} - {self.request_data} - {self.response_data}"
+        return f"Log {self.id} - {self.username} - {self.model} - {self.timestamp} - {self.request_data} - {self.response_data}"
--- api_logs/schema.py
+++ api_logs/schema.py
@@ -5,23 +5,39 @@ from .models import APILog
 class APILogType(DjangoObjectType):
     class Meta:
         model = APILog  
+        fields = "__all__"
 
 class Query(graphene.ObjectType):
-    all_logs = graphene.List(APILogType)
+    all_logs = graphene.List(
+        APILogType,
+        username=graphene.String(required=False),
+        model=graphene.String(required=False),
+    )
 
-    def resolve_all_logs(self, info):
-        return APILog.objects.all()
+    def resolve_all_logs(self, info, username=None, model=None):
+        queryset = APILog.objects.all()
+        if username:
+            queryset = queryset.filter(username=username)
+        if model:
+            queryset = queryset.filter(model=model)
+        return queryset
 
 class CreateAPILog(graphene.Mutation):
     class Arguments:
         username = graphene.String()
+        model = graphene.String()  
         request_data = graphene.String()
         response_data = graphene.String()
 
     log = graphene.Field(APILogType)
 
-    def mutate(self, info, username, request_data, response_data):
-        log = APILog.objects.create(username=username, request_data=request_data, response_data=response_data)
+    def mutate(self, info, username, model, request_data, response_data):
+        log = APILog.objects.create(
+            username=username,
+            model=model,
+            request_data=request_data,
+            response_data=response_data,
+        )
         return CreateAPILog(log=log)
 
 class Mutation(graphene.ObjectType):
